78f94b0b729c0f8a244dcfa8bea2fba6d7115e1f
김정태 <tae101010@naver.com>
2022-04-07 17:18:31 +0900

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 김정태 on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
